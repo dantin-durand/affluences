1d5ef2fee164ae8d0ea987790831e7da5e6b9526
Dantin Durand <durand.dantin.pro@gmail.com>
2021-02-28 15:50:35 +0100

    debug

--- resources/views/reservation/reservation.blade.php
+++ resources/views/reservation/reservation.blade.php
@@ -32,9 +32,9 @@
 
                 <?php
                 for ($i = $reservation_timetable[0]; $i <= $reservation_timetable[1]; $i += $reservation_duration) {
-                    print_r($existing_reservations);
+                    dd($existing_reservations);
                     if (isset($existing_reservations[$i])) {
-                        print_r("second -> " . $existing_reservations);
+                        dd("second -> " . $existing_reservations);
                         if ($existing_reservations[$i] >= $reservation_limit) {
 
                             echo
